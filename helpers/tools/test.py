2e076974159f6fd237fc2dce69165cc82141a0ec
Peter <mail@peterhansen.io>
2025-06-09 14:31:12 -0400

    Remove pytest-cov check

--- helpers/tools/test.py
+++ helpers/tools/test.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import argparse
-import importlib.util
 from helpers.utils import logger, run_command
 from helpers.tools import tool, SubParser
 
@@ -13,13 +12,7 @@ VENV_WANT = "test"
 def run(args: argparse.Namespace) -> None:
   logger.debug("extra args: %s", args.extra)
 
-  cmd = ["pytest"]
-  if importlib.util.find_spec("pytest_cov") is None:
-    logger.warning("pytest-cov not installed; running without coverage")
-    cmd.extend(["-o", "addopts="])
-
-  cmd.extend(args.extra)
-  run_command(cmd)
+  run_command(["pytest", *args.extra])
 
 
 @tool("test")
